ee5c1776f5952e31599aaa1ae24330d682daffc1
Atharv Phospe <atharvphopse08@gmail.com>
2025-07-26 10:02:06 +0530

    new repo

---
+++ assets/js/js_script.js
@@ -0,0 +1,23 @@
+let a=12;
+let b=30;
+console.log(typeof a)
+
+
+let i=0;
+let x="atharv";
+for(i=1;i<10;i++)
+    {
+        console.log(x);
+    }
+
+    for(i=x.length-1;i<=0;i--)
+    {
+        process.stdout.write(x[i]);
+    }
+
+
+    
+
+
+
+
